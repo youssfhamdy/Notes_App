e6af925c3b7bd869965aab9146a43662dd14e096
youssfhamdy <youssf.hamdy7200@gmail.com>
2022-12-09 13:57:16 +0200

    create_notes_app

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class notes_view extends StatelessWidget {
+  const notes_view({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
